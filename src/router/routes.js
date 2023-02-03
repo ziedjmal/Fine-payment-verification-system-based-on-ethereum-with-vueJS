7e26c28caf19fda0d944d5ab3ddca2040904e8fe
zied jmal <zied.jmal@enis.tn>
2023-02-03 18:18:30 +0100

    first Commit

---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+  ,{
+    path: '/abc',
+    component: () => import('src/pages/PageTest.vue')
+
+
+  }
+]
+
+export default routes
